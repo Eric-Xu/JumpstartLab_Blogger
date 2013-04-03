2983979cca7968602893211441ac307cb17c139c
lisacf_ericXu <lisacf@gmail.com>
2013-04-03 10:10:18 -0700

    fix articles controller mistake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
-  def index
-    @articles = Article.all
-  end
 
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  def new
-
-  end
 end
