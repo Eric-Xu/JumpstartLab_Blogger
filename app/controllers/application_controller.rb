aaa86453e7e5d8394c48458d1be048df7fd748db
lisacf_ericXu <lisacf@gmail.com>
2013-04-03 09:55:21 -0700

    css style and article link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+
+  end
 end
