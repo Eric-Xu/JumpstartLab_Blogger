b9f2d3bf8621007fe99a69d1329e4a4bf2c2eead
lisacf_ericXu <lisacf@gmail.com>
2013-04-03 17:48:36 -0700

    finish authentication using sorcery gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Blogger::Application.routes.draw do
   resources :articles do
     resources :comments
-
   end
+
   resources :tags
+  resources :authors
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  match 'login'  => 'author_sessions#new'
+  match 'logout' => 'author_sessions#destroy'
+
   root to: 'articles#index'
 end
